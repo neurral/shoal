819c1f73e18dfe6f2b4b6dd49d6bc5d683400a6e
Lee Alexis <john20xdoe@users.noreply.github.com>
2020-08-08 21:50:16 +0800

    gh-pages to master (#1)
    * added live demo
    
    * added live demo
    
    * added github ribbon
    
    * fixed github ribbon
    
    * Spellchecking
    
    Just fixes some small grammatical stuff.
    
    * .
    
    * removed noise
    
    * fixed bug: certain mutations caused negative mass
    
    * Update README.md
    
    * fixed typos
    
    * Fix broken Markdown headings
    
    * fixed typos
    
    * fixed typos
    
    * Added composer.json for Heroku to recognize buildpacks
    
    * Renamed 'vendor' folder to something not reserved for heroku
    
    * Hide Fork Me link to become standalone website for Zenn-like bookmarking
    
    * html font sizes and layout
    
    * MOdified fish colors, food look, sea variables
    
    * Fixed canvas unicode support
    
    Co-authored-by: Juan Cazala <juancazala@gmail.com>
    Co-authored-by: Juan Cazala <cazala@mail.sfsu.edu>
    Co-authored-by: Richard Littauer <richard.littauer@gmail.com>
    Co-authored-by: Santiago Castro <santi.1410@hotmail.com>

--- src/food.js
+++ src/food.js
@@ -1,62 +1,70 @@
 // Food constructor
-function Food(x, y, amount)
-{
-	// food properties
-	this.location = new Vector(x,y);
-	this.velocity = new Vector(Math.random() * 2 - 1, Math.random() * 2 - 1);
-	this.energy = amount;
-	this.radius = 5;
-	this.dead = false;
+function Food(x, y, amount) {
+  // food properties
+  this.location = new Vector(x, y);
+  this.velocity = new Vector(Math.random() * 2 - 1, Math.random() * 2 - 1);
+  this.energy = amount;
+  this.radius = 5;
+  this.dead = false;
 
-	// helper
-	this.TWO_PI =  Math.PI * 2;
+  // helper
+  this.TWO_PI = Math.PI * 2;
 }
 
 // food methods
 Food.prototype = {
+  // draw the food
+  draw: function (ctx) {
+    if (this.radius < 0) return;
+    var radiusText = parseInt(this.radius);
+    var foodFontSize = Math.max(5, Math.max(radiusText, 10));
+    var centerRadius = this.radius / 2;
 
-	// draw the food
-	draw: function(ctx)
-	{
-		if (this.radius < 0) return;
+    ctx.beginPath();
+    ctx.arc(this.location.x, this.location.y, this.radius, 0, this.TWO_PI);
 
-		ctx.beginPath();
-		ctx.arc(this.location.x, this.location.y, this.radius, 0, this.TWO_PI);
+    var old = ctx.globalAlpha;
+    ctx.globalAlpha = 0.5;
+    ctx.fillStyle = '#009966';
+    ctx.fill();
+    ctx.font = foodFontSize + 'px Verdana';
+    ctx.fillStyle = '#000000';
+    ctx.globalAlpha = this.energy > 0 ? 0.5 : this.radius / 100;
 
-		var old = ctx.globalAlpha;
-		ctx.globalAlpha = .5
-		ctx.fillStyle = "#eeeeee";
-		ctx.fill();
-		ctx.font = '14px Verdana';
-		ctx.fillStyle = "#000000";
-		ctx.globalAlpha = this.energy > 0 ? .5 : this.radius/100;
-		ctx.fillText("FOOD",this.location.x - 20,this.location.y + 5);
-		ctx.globalAlpha = old;
-	},
+    var foodUnicode = '\u{1F33F}'; //🌿
+    ctx.fillText(
+      foodUnicode,
+      this.location.x - centerRadius,
+      this.location.y + centerRadius
+    );
+    ctx.globalAlpha = old;
+  },
 
-	// update the food
-	update: function(world)
-	{
-		// calculate radious according to the ammount of energy (i.e. ammount of food)
-		var target = this.energy > 0 ? this.energy + 50 : 0;
-		this.radius += (target - this.radius) / 5;
+  // update the food
+  update: function (world) {
+    // calculate radious according to the amount of energy (i.e. amount of food)
+    var target = this.energy > 0 ? this.energy + 50 : 0;
+    this.radius += (target - this.radius) / 5;
 
-		// move food
-		this.location.add(this.velocity);
+    // move food
+    this.location.add(this.velocity);
 
-		// if food goes out of the boundaries of the sea, kill it
-		if (this.location.x > world.width || this.location.x < 0 || this.location.y > world.height || this.location.y < 0)
-			this.energy = 0;
-		
-		// die 
-		if (this.radius < 5)
-			this.dead = true;
-	},
+    // if food goes out of the boundaries of the sea, kill it
+    if (
+      this.location.x > world.width ||
+      this.location.x < 0 ||
+      this.location.y > world.height ||
+      this.location.y < 0
+    )
+      this.energy = 0;
 
-	// get a bite by a fish
-	eatenBy: function(fish)
-	{
-		this.energy -= fish.bite;
-		fish.energy += fish.bite;
-	}
-}
+    // die
+    if (this.radius < 5) this.dead = true;
+  },
+
+  // get a bite by a fish
+  eatenBy: function (fish) {
+    this.energy -= fish.bite;
+    fish.energy += fish.bite;
+  },
+};
